936b55d0d9dab44d0194781484101a2110c87103
Chris Acedo <tempeacedo@yahoo.com>
2020-05-14 14:40:15 -0700

    CSS/Cleaned JS

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -128,15 +128,21 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-         console.log('Page created! Check out index.html in this directory to see it!');
-     });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-
 
 //const profileDataArgs = process.argv.slice(2, process.argv.length);
 //const [name, github] = profileDataArgs;
