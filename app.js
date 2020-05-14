843d979089922d670e36d64e77a6a77c85ceca5f
Chris Acedo <tempeacedo@yahoo.com>
2020-05-14 06:49:28 -0700

    arrays to html

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -126,18 +128,15 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+         console.log('Page created! Check out index.html in this directory to see it!');
+     });
   });
 
 
-
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
 //const profileDataArgs = process.argv.slice(2, process.argv.length);
 //const [name, github] = profileDataArgs;
-
-//fs.writeFile('index.html', generatePage(name, github), err => {
-  //if (err) throw err;
-
-  //console.log('Portfolio complete! Check out index.html to see the output!');
-//});
